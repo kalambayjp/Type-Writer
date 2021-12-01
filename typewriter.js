aeb0ecbc78f601335c423837a1eae64f27b430e3
JP <kalambayjp@gmail.com>
2021-12-01 11:32:28 -0800

    fixed bug, now function on creates one new line

--- typewriter.js
+++ typewriter.js
@@ -2,13 +2,12 @@ const sentence = 'hello there from lighthouse labs';
 var increment = 0;
 
 for (const char of sentence) {
-  
   setTimeout(() => {
     process.stdout.write(char);
   }, increment)
   increment += 50;
-  setTimeout(() => {
-    process.stdout.write('\n');
-  }, 1650)
-  
 };
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, increment)
